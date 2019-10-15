c70dfc84c2f782c44233b7fac334ce8bc29655b4
Ester Abram <esterabram@hotmail.com>
2019-10-15 15:13:16 +0200

    Update

--- pointLISA/AIM.py
+++ pointLISA/AIM.py
@@ -938,7 +938,7 @@ class AIM():
             try:
                 t_end = kwargs['t_end']
             except KeyError:
-                t_end=np.float(self.data.length_calc )
+                t_end=np.float(self.data.length_calc)
             method='solve'
             lim=parameters.FOV
             #ret='angx_arm_tele_rec'
